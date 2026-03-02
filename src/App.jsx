41c589dffdae3bbfdf6d0fcca84f146aa4438950
Bramasta <bramastasaka9@gmail.com>
2026-03-02 09:11:30 +0700

    setup landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Landing from "./pages/LandingPages"
 import Dashboard from "./pages/Dashboard"
 
 export default function App() {
-  return <Dashboard />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
---
+++ src/pages/LandingPages.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+export default function Landing() {
+  return (
+    <div className="bg-white text-gray-900">
+      <section className="min-h-screen flex flex-col justify-center items-center text-center px-6 bg-linear-to-br from-blue-500 via-white to-purple-50">
+        <h1 className="text-5xl md:text-6xl font-bold leading-tight max-w-4xl">
+          The Smart Productivity Hub for Modern Students
+        </h1>
+
+        <p className="mt-6 text-lg text-gray-600 max-w-2xl">
+          Organize tasks, track goals, and boost academic performance — all in
+          one intelligent dashboard.
+        </p>
+
+        <div className="mt-8 flex gap-4">
+          <Link
+            to="/dashboard"
+            className="px-6 py-3 rounded-lg bg-black text-white hover:opacity-80 transition"
+          >
+            Try Demo
+          </Link>
+
+          <button className="px-6 py-3 rounded-lg border border-gray-300 hover:bg-gray-100 transition">
+            Learn More
+          </button>
+        </div>
+      </section>
+    </div>
+  );
+}
